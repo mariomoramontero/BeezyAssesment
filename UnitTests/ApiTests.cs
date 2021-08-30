3fb111f3d14687daf172fdd1d8f887b332af1fed
mariomora <yo.mario@gmail.com>
2021-08-30 09:17:30 +0200

    some unit tests

---
+++ UnitTests/ApiTests.cs
@@ -0,0 +1,19 @@
+﻿using BeezyAssesment.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        [TestMethod]
+        public async void GetComicsNotNull()
+        {
+            var comicWrap = await MarvelClient.Instance.GetComics();
+
+            Assert.IsNotNull(comicWrap,"Api retrieves results");
+            Assert.AreNotEqual(comicWrap?.data.count, 0);
+        }
+    }
+}
---
+++ UnitTests/VMTests.cs
@@ -0,0 +1,25 @@
+﻿using BeezyAssesment.Services;
+using BeezyAssesment.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    class VMTests
+    {
+        [TestMethod]
+        public void GetComicsFilteredNotNull()
+        {
+            var vm = new ComicsViewModel();
+            vm.FilterTitleStartsWith="hulk";
+            vm.LoadItemsCommand.Execute(null);
+            Assert.IsNotNull(MarvelClient.Instance.CatchedComicData, "Filtered results loaded");
+            
+        }
+    }
+}
